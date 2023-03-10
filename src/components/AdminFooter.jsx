9051b51bbfa6dcb10390e5781bf133c01db738d4
aobichkin <aobichkin@uzkassa.uz>
2023-03-10 17:28:32 +0500

    Changed logo and year in footer

--- src/components/AdminFooter.jsx
+++ src/components/AdminFooter.jsx
@@ -1,14 +1,14 @@
-import VskLogo from '../img/vsk-logo.svg';
+import VskLogo from '../img/vsk-logo.png';
 const AdminFooter = () => {
     return (
         <div className="admin-footer">
             <div className="container">
                 <div className="row">
                     <div className="col-6 cm-col">
-                        <p>1992–2021 Страховое акционерное общество «ВСК»<br />Россия, Москва, 121552, ул. Островная, 4</p>
+                        <p>1992–{new Date().getFullYear()} Страховое акционерное общество «ВСК»<br />Россия, Москва, 121552, ул. Островная, 4</p>
                     </div>
                     <div className="col-6 cm-col right">
-                        <img src={VskLogo} alt="vsk-logo" />
+                        <img style={{ maxWidth: 150 }} src={VskLogo} alt="vsk-logo" />
                     </div>
                 </div>
             </div>
